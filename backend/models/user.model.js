380fa2d5575f247b9b1eab115e71005de394b4d2
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-08-31 21:46:43 +0530

    feat: enhance userSchema with password hashing and token generation methods

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,14 +1,18 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema(
   {
-    name: {
+    fullName: {
       type: String,
       required: [true, "Name is required"],
     },
     email: {
       type: String,
       required: [true, "Email is required"],
+      unique: true,
+      trim: true,
     },
     password: {
       type: String,
@@ -23,8 +27,53 @@ const userSchema = new Schema(
       default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
   },
   { timeStamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
