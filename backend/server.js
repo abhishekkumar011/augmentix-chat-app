7162a2ff5afdd7b930d9e814902562d7c59336e9
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-08-31 15:30:21 +0530

    feat: implement MongoDB connection using Mongoose with error handling

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
 import { app } from "./app.js";
 import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+const port = process.env.PORT || 8000;
 
 dotenv.config({
   path: "./.env",
 });
 
-const port = process.env.PORT || 8000;
-
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
-app.listen(port, () => {
-  console.log(`Server at http://localhost:${port}`);
-});
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR : ", error);
+    });
+    app.listen(port, () => {
+      console.log(`Server at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed !!! ", err);
+  });
