823cb7126befe41a28accfe1127a86d6e1447e94
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-09-01 02:23:23 +0530

    feat: implement user-register endpoint

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,47 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if ([fullName, email, password].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = await User.findOne({ email });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with this email already exist");
+  }
+
+  const avatarLocalPath = req.file?.path;
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  const user = await User.create({
+    fullName,
+    email,
+    password,
+    avatar: {
+      public_id: avatar?.public_id || "",
+      url: avatar?.url || "",
+    },
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered Successfully"));
+});
+
+export { registerUser };
